7eb77c8054f01ec464c74c8cce68f75366167309
agp123 <43734755+agp123@users.noreply.github.com>
2019-01-08 12:18:59 -0500

    Add files via upload

---
+++ Lesson 10/problem1.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Life Decision Bot: ')
+print()
+weather = input('Are you bored? ')
+if weather == 'yes':
+	print('Read a book or listen to music.')
+	print('Or do something productive and do some work.' )
+else:
+	print('Good Im glad you arent bored!')
+print('-'*60)
---
+++ Lesson 10/problem3.py
@@ -0,0 +1,10 @@
+print('-'*60)
+print('Welcome to CLUB425, the most lit club in downtown ACTvF. Before you can enter, I need you to answer some quetsions... ')
+print()
+age = input('What is your age today? ')
+age = int(age)
+if age >= 21:
+	print('Cool, come on in.')
+else:
+	print("Back up, this club is 21+" )
+print('-'*60)
